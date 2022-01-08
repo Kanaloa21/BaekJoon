e6fb89e3922a7432d8329e607a8f23c5dcef2388
Kanaloa <kjwkjw1104@gmail.com>
2022-01-08 23:34:23 +0900

    add 1065.py

---
+++ Solve/Silver/1065.py
@@ -0,0 +1,17 @@
+#import time
+def sol(n):
+    res = n
+    if n > 99:
+        for i in range(99, n+1):
+            stri = str(i)
+            d = int(stri[0]) - int(stri[1])
+            for j in range(len(stri)-1):
+                if d != int(stri[j]) - int(stri[j+1]): 
+                    res -= 1
+                    break
+    else: pass
+    return res
+#start = time.time()
+n = int(input())
+print(sol(n))
+#print(f'Run time: {time.time() - start}')
