4d51cd3666a8d69bb380ef6c48d32b9cc6418ef2
Kanaloa21 <kjwkjw1104@gmail.com>
2022-01-13 23:24:34 +0900

    solve bronze

--- Solve/Bronze/2475.py
+++ Solve/Bronze/2475.py
@@ -1,6 +1,2 @@
 serial_number = list(map(int, input().split()))
-sum = 0
-for i in serial_number:
-    sum += i**2
-unique_number = sum % 10
-print(unique_number)
+print(sum(i**2 for i in serial_number) % 10)
